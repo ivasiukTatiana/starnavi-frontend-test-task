0cf47ee26569351686c18a02a8354e81ffe31ec7
Tetiana Mysiuk <tivasyuk@i.ua>
2020-02-10 21:25:46 +0200

    added getData method

--- src/App.js
+++ src/App.js
@@ -205,23 +205,22 @@ export default class App extends Component {
       message: message,
     });
 
-    fetch(url + urlWinners, {
-      method: "POST",
-      body: JSON.stringify(objWinner),
-      headers: {
-        "Content-Type": "application/json"
-      }})
-      .then(response => {
-        return response.json()
-      })
-      .then(response => {
+    (async () => {
+      try {
+        let response = await fetch(url + urlWinners, {
+          method: "POST",
+          body: JSON.stringify(objWinner),
+          headers: {
+            "Content-Type": "application/json"
+          }});
+        let newWinners = await response.json();
         this.setState({
-          winners: response
+          winners: newWinners
         });
-      })
-      .catch(err => {
+      } catch(err) {
         console.log(err);
-      })
+      }
+    })();
   }
 
   /**
